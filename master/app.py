db8272bc95e3edc0908d86c4d920c1b5bf83a23c
Zhong-AI1 <afs693066@gmail.com>
2025-05-15 23:34:32 +0800

    Update app.py

--- master/app.py
+++ master/app.py
@@ -2,10 +2,10 @@ import streamlit as st
 from openai import OpenAI
 import os
 
-# 從環境變數讀取金鑰
-os.environ["OPENAI_API_KEY"] = "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"  # 如果沒在系統設定，這樣臨時設定也行
+# 從環境變數讀取 API KEY
+api_key = os.getenv("OPENAI_API_KEY")
 
-client = OpenAI()
+client = OpenAI(api_key=api_key)
 
 st.title("簡易測試")
 
