a10b8d20d906f3e0ad89f8b5d589509cf51df96a
Zhong-AI1 <afs693066@gmail.com>
2025-05-15 23:43:41 +0800

    Update app.py

--- master/app.py
+++ master/app.py
@@ -5,10 +5,6 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 
-load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
-st.write("API Key 前10碼:", api_key[:10] if api_key else "沒有讀到金鑰")
-
 api_key = os.getenv("OPENAI_API_KEY")
 if not api_key:
     st.error("請先在 .env 設定 OPENAI_API_KEY")
